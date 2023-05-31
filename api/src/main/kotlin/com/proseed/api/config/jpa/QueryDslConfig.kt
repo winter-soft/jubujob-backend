08a78bfe8924d6caa4d42422612eb403fda0429b
ghdcksgml1 <ghdcksgml2@naver.com>
2023-05-31 16:43:41 +0900

    feat: QueryDsl Configuration

---
+++ api/src/main/kotlin/com/proseed/api/config/jpa/QueryDslConfig.kt
@@ -0,0 +1,20 @@
+package com.proseed.api.config.jpa
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class QueryDslConfig {
+    @PersistenceContext
+    private val em: EntityManager? = null
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(em)
+    }
+}
